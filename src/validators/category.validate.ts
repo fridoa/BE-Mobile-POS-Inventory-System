e0abc0ff44dcb5c6c71fe825048e53b46ba042d4
fridoa <fridoafriyanto3@gmail.com>
2026-05-15 01:25:51 +0700

    feat: add imageUrl and imageFileId fields to category validation schema

--- src/validators/category.validate.ts
+++ src/validators/category.validate.ts
@@ -2,6 +2,8 @@ import * as Yup from "yup";
 
 export const categorySchema = Yup.object().shape({
   name: Yup.string().required("Category name is required").trim(),
+  imageUrl: Yup.string().nullable(),
+  imageFileId: Yup.string().nullable(),
 });
 
 export type TCategory = Yup.InferType<typeof categorySchema>;
